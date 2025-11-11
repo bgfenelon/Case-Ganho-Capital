74855390dd0a7bd110bd495ee8123ff890a3a629
Bianca Fenelon <biafenelon2014@gmail.com>
2025-11-10 22:11:12 -0300

    ajustando código

--- src/tests/test_calcular_ganho_capital.py
+++ src/tests/test_calcular_ganho_capital.py
@@ -16,12 +16,23 @@ class TestCalcularGanhoCapital(unittest.TestCase):
 
     def test_case_1(self):
         entrada_dados = [{"operation":"buy", "unit-cost":10.00, "quantity": 100},
-{"operation":"sell", "unit-cost":15.00, "quantity": 50},
-{"operation":"sell", "unit-cost":15.00, "quantity": 50}]
+        {"operation":"sell", "unit-cost":15.00, "quantity": 50},
+        {"operation":"sell", "unit-cost":15.00, "quantity": 50}]
 
         self.calc.processar_operacoes(entrada_dados)
 
         resultado_esperado = [{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]
+
+        self.assertEqual(self.calc.get_lista_imposto(), resultado_esperado)
+    
+    def test_case_2(self):
+        entrada_dados = [{"operation":"buy", "unit-cost":10.00, "quantity": 10000},
+{"operation":"sell", "unit-cost":20.00, "quantity": 5000},
+{"operation":"sell", "unit-cost":5.00, "quantity": 5000}]
+
+        self.calc.processar_operacoes(entrada_dados)
+
+        resultado_esperado = [{"tax": 0.0},{"tax": 10000.0},{"tax": 0.0}]
 
         self.assertEqual(self.calc.get_lista_imposto(), resultado_esperado)
 
